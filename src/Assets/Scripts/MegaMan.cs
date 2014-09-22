efaa6cdcf415faa3de47f83b03a6a4d0cce67e6e
Oleg Andrushko <nubick@gmail.com>
2014-09-22 11:25:16 +0800

    Initial commit.

---
+++ src/Assets/Scripts/MegaMan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MegaMan : MonoBehaviour
+    {
+
+        //public float PositionX;
+
+	
+        public void Update () 
+        {
+            //transform.position = new Vector2(PositionX, transform.position.y);
+	    
+        }
+
+        public void SetPositionX(float x)
+        {
+            transform.position = new Vector2(x, transform.position.y);
+        }
+    }
+}
